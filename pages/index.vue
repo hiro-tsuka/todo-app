82521d1049e663d6d92f6250f4a05001310c71dd
hiro-tsuka <59140769+hiro-tsuka@users.noreply.github.com>
2023-07-02 23:25:53 +0900

    add

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,6 @@ Copyright © 2023 Iizuka All rights reserved.
 import { ref, computed } from 'vue';
 import { storeToRefs } from "pinia";
 import { useTodos } from '~/store/todo';
-import  todoItem  from '../components/todoItem.vue';
 const todosStore = useTodos();
 const {addTodo, removeTodo} = todosStore;
 // ストアの値を参照するためのref
@@ -31,7 +30,7 @@ const filteredTodos = computed(() => {
 </script>
 
 <template>
-  <!-- preeventの設定で送信後の自動リロードをキャンセル、addTodoでstoreに新規追加＆newTodoを初期化 -->
+  <!-- preventの設定で送信後の自動リロードをキャンセル、addTodoでstoreに新規追加＆newTodoを初期化 -->
   <form @submit.prevent="addTodo(newTodo); newTodo = ''">
     <!-- 入力された値のとnewtodoRefを同期 -->
     <input v-model="newTodo">
